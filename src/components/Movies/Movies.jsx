1629a7104afac9d656b58c45eda7c2d18c4a9494
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-11-26 15:36:14 +0200

    Displayed movies from API

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,14 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 function Movies() {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'Nice, you broke the site.';
 
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 }
