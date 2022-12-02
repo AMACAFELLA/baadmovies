e1b5ba27629714e9cb847b579de01b50b49fd64b
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-12-02 14:04:06 +0200

    Displaying all the movies with title and image

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -8,6 +8,7 @@ export default makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
     marginLeft: '240px',
+
     [theme.breakpoints.down('sm')]: {
       marginLeft: 0,
       flexWrap: 'wrap',
