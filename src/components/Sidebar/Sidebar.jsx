a032ba2a61dab1513bb14bef6e1b4fd83a0d6aab
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2022-12-13 09:44:50 +0200

    added select category or genre code

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  ListItemIcon,
+  Box,
+  CircularProgress,
+} from '@mui/material';
 import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
 import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
 import genreIcons from '../../Assets/genres';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const redLogo = 'https://i.postimg.cc/D0PkxtBt/1.png';
 const blueLogo = 'https://i.postimg.cc/76xLM81T/3.png';
@@ -19,6 +30,7 @@ function Sidebar({ setMobileOpen }) {
   const theme = useTheme();
   const classes = useStyles();
   const { data, isFetching } = useGetGenresQuery();
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -35,32 +47,44 @@ function Sidebar({ setMobileOpen }) {
         <ListSubheader>Categories</ListSubheader>
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
-            <ListItem onClick={() => {}} button>
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
+
               <ListItemIcon>
-                <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
+                <img
+                  src={genreIcons[label.toLowerCase()]}
+                  className={classes.genreImage}
+                  height={30}
+                />
               </ListItemIcon>
               <ListItemText primary={label} />
             </ListItem>
           </Link>
         ))}
       </List>
+
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
         {isFetching ? (
-          <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+          <Box display="flex" justifyContent="center">
             <CircularProgress />
           </Box>
-        ) : data.genres.map(({ name, id }) => (
-          <Link key={name} className={classes.links} to="/">
-            <ListItem onClick={() => {}} button>
-              <ListItemIcon>
-                <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
-              </ListItemIcon>
-              <ListItemText primary={name} />
-            </ListItem>
-          </Link>
-        ))}
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link key={name} className={classes.links} to="/">
+              <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
+                <ListItemIcon>
+                  <img
+                    src={genreIcons[name.toLowerCase()]}
+                    className={classes.genreImage}
+                    height={30}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
+            </Link>
+          ))
+        )}
       </List>
     </>
   );
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,18 +7,19 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
-    //* Get Genres
+
     getGenres: builder.query({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
-    //* Get Movies by [Type]
+
+    //* Popular Movies with Vote Average <= 5 in the United States of America
     getMovies: builder.query({
-      query: () => `movie/popular?page${page}&api_key=${tmdbApiKey}`,
-    }),
-  }),
+      query: () => `movie/popular?page${page}&vote_average.lte=5&language=en&with_original_language=en&api_key=${tmdbApiKey}`,
+   		}),
+  	}),
 });
 
 export const {
   useGetMoviesQuery,
   useGetGenresQuery,
-} = tmdbApi.hooks;
+} = tmdbApi;
